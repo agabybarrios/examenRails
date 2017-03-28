451482c130a07a55eaf681f75a6e3b92e5794165
agabybarrios <agabybarrios@gmail.com>
2017-03-27 22:25:11 -0600

    created partial view in bootstrap layout

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,11 +1,11 @@
 class ExperimentsController < ApplicationController
-  layout "bootstrap", :only => :page2
+
   def page1
 
   end
 
   def page2
-
+    render layout: "bootstrap"
   end
 
   def page3
