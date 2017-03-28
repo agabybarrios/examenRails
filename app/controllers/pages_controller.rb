d5bdbbab7710e372055d335021dcb738b20e9088
agabybarrios <agabybarrios@gmail.com>
2017-03-27 21:32:20 -0600

    changed text when form is submited

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,12 +11,12 @@ class PagesController < ApplicationController
 
   def save_votossuperman
     Votossuperman.create(name: params[:name], email: params[:email])
-    redirect_to pages_batmanvssuperman_path, notice: 'El usuario se ha creado exitosamente'
+    redirect_to pages_batmanvssuperman_path, notice: 'Tu voto ha sido registrado'
   end
 
   def save_votosbatman
     Votosbatman.create(name: params[:name], email: params[:email])
-    redirect_to pages_batmanvssuperman_path, notice: 'El usuario se ha creado exitosamente'
+    redirect_to pages_batmanvssuperman_path, notice: 'Tu voto ha sido registrado'
   end
 
 end
