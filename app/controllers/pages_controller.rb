6fa29dc3688606855abee6d19ddcd85010d981c6
agabybarrios <agabybarrios@gmail.com>
2017-03-27 21:16:20 -0600

    Crecion de modelo y formulario

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,15 @@ class PagesController < ApplicationController
 
   def batmanvssuperman
   end
+
+  def save_votossuperman
+    Votossuperman.create(name: params[:name], email: params[:email])
+    redirect_to pages_x_path, notice: 'El usuario se ha creado exitosamente'
+  end
+
+  def save_votosbatman
+    Votosbatman.create(name: params[:name], email: params[:email])
+    redirect_to pages_x_path, notice: 'El usuario se ha creado exitosamente'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   get 'pages/batmanvssuperman'
 
+  post 'pages/save_votossuperman'
+
+  post 'pages/save_votosbatman'
+
   root 'pages#batmanvssuperman'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
