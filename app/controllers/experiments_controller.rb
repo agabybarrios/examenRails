8e016ef4975941bea34a86f432a503761ed7cdc1
agabybarrios <agabybarrios@gmail.com>
2017-03-27 21:59:03 -0600

    created bootstrap layout for page2

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,10 +1,14 @@
 class ExperimentsController < ApplicationController
+  layout 'bootstrap'
   def page1
+    layouts = false
   end
 
   def page2
+
   end
 
   def page3
+    layouts = false
   end
 end
