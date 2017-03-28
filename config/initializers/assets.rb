f2f97cbb04ac770927f0ea0cfd5637aa74ab890a
agabybarrios <agabybarrios@gmail.com>
2017-03-27 21:44:52 -0600

    added image in vendor/imgs and showed it in page1

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,8 +4,10 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
+Rails.application.config.assets.paths << Rails.root.join("vendor", "imgs")
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
+Rails.application.config.assets.precompile += %w( landscape.jpg )
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
